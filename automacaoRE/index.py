ded7976ea96316442faa061ce45613cdf0adf563
Broyz <contato.brunooliveiramarques@gmail.com>
2021-07-23 20:40:39 -0300

    Atualização da automação RE

--- automacaoRE/index.py
+++ automacaoRE/index.py
@@ -4,30 +4,34 @@ import time
 navegador = webdriver.Chrome()
 
 navegador.get("https://leomadeiras.routeasy.com.br/#!/dashboard")
-time.sleep(3)
+time.sleep(5)
 
 username = navegador.find_element_by_xpath('//*[@id="page-top"]/section/div/div/div/div/div[1]/form[1]/div[1]/input')
 password = navegador.find_element_by_xpath('//*[@id="page-top"]/section/div/div/div/div/div[1]/form[1]/div[2]/input')
 username.send_keys("bruno.marques@leomadeiras.com.br")
 password.send_keys("Broyz@1998")
 navegador.find_element_by_xpath('//*[@id="page-top"]/section/div/div/div/div/div[1]/form[1]/div[3]/button').click()
-time.sleep(3)
-
+time.sleep(5)
 
 qtde = 20
 aux = 2
+
+menu = '/html/body/section/ui-view/div/div/div[1]/div[1]/div/div[3]/span'
+fecharMenu = 'body > div.modal.fade.ng-isolate-scope.custom-options-modal-relatory.in > div > div > div > div.modal-header-options > a'
+rotas = 'body > div.modal.fade.ng-isolate-scope.custom-options-modal-relatory.in > div > div > div > div.modal-body.ng-scope > div > div > div:nth-child(10)'
+servicos = 'body > div.modal.fade.ng-isolate-scope.custom-options-modal-relatory.in > div > div > div > div.modal-body.ng-scope > div > div > div:nth-child(7)'
 
 while aux < qtde:    
     navegador.get("https://leomadeiras.routeasy.com.br/#!/routings/list")
     time.sleep(3)
     navegador.find_element_by_xpath('/html/body/section/ui-view/div/div/div[' + str(aux) + ']').click()
     time.sleep(3)
-    navegador.find_element_by_xpath('/html/body/section/ui-view/div/div/div[1]/div[1]/div/div[3]/span').click()
+    navegador.find_element_by_xpath(menu).click()
     time.sleep(3)
-    navegador.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/div[10]').click()
+    navegador.find_element_by_css_selector(rotas).click()
     time.sleep(3)
-    navegador.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/div[7]').click()
+    navegador.find_element_by_css_selector(servicos).click()
     time.sleep(3)
-    navegador.find_element_by_css_selector('body > div.modal.fade.ng-isolate-scope.custom-options-modal-relatory.in > div > div > div > div.modal-header-options > a').click()
-    time.sleep(5)
+    navegador.find_element_by_css_selector(fecharMenu).click()
+    time.sleep(10)
     aux += 1
---
+++ automacaoWPP/script.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib
+
+navegador = webdriver.Chrome()
+navegador.get("https://web.whatsapp.com/")
+
+
+while len(navegador.find_elements_by_id("side")) < 1:
+    time.sleep(1)
+
+# já estamos com o login feito no whatsapp web
+numero = "+5511989420494"
+texto = "Via: Daum"
+x = 1
+
+while x < 20: 
+
+    msg = str(texto)
+
+    link = f"https://web.whatsapp.com/send?phone={numero}&text={msg}"
+
+    navegador.get(link)
+    while len(navegador.find_elements_by_id("side")) < 1:
+        time.sleep(2)
+    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]/button').click()
+    time.sleep(2)
+
+    x += 1 
